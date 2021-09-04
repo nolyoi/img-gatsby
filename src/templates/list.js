4f6c9ef0ff33d364341b8fa563ced85c1ed2140a
nolyoi <n.mayersky@hey.com>
2021-09-04 15:45:05 -0500

    Add blog

--- src/templates/list.js
+++ src/templates/list.js
@@ -27,20 +27,3 @@ class ListTemplate extends React.Component {
 }
 
 export default ListTemplate
-
-export const pageQuery = graphql`
-  query ListPageBySlug($slug: String!) {
-    site {
-      siteMetadata {
-        title
-      }
-    }
-    markdownRemark(fields: { slug: { eq: $slug } }) {
-      id
-      html
-      frontmatter {
-        title
-      }
-    }
-  }
-`
