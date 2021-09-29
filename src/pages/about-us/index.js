4924bf42451725dbb86ea10a3bd70306d5e1284b
nolyoi <n.mayersky@hey.com>
2021-09-29 12:46:37 -0500

    Update about and mary.

--- src/pages/about-us/index.js
+++ src/pages/about-us/index.js
@@ -8,8 +8,6 @@ import { Link } from 'gatsby';
 import sal from 'sal.js';
 
 function AboutUs() {
-    const [showDesai, setShowDesai] = React.useState(false);
-    const [showChristina, setShowChristina] = React.useState(false);
     React.useEffect(
         () => {
             sal();
@@ -173,115 +171,22 @@ function AboutUs() {
                                 data-sal-delay="400"
                                 data-sal-duration="1500"
                                 data-sal-easing="ease-out-quint" className="w-full max-w-xs text-center">
-                                <img className="object-cover object-center w-full h-48 mx-auto rounded-lg" src="../../mary.jpeg" alt="avatar" />
+                                <Link to="/about-us/mary-szymaszek"><img className="object-cover object-center w-full h-48 mx-auto rounded-lg" src="../../mary.jpeg" alt="avatar" /></Link>
                                 <div className="mt-2">
                                     <h3 className="text-lg font-medium text-gray-700 dark:text-gray-200">Mary Szymaszek</h3>
                                     <p className="mt-0 font-medium text-gray-600 dark:text-gray-300">Nurse/Office Manager</p>
-                                    <a className="text-orange-500 inline-flex items-center">Learn More
+                                    <Link to="/about-us/mary-szymaszek" className="text-orange-500 inline-flex items-center">Learn More
                                         <svg className="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2" fill="none" strokeLinecap="round" strokeLinejoin="round">
                                             <path d="M5 12h14"></path>
                                             <path d="M12 5l7 7-7 7"></path>
                                         </svg>
-                                    </a>
+                                    </Link>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </section>
             </div>
-            {showDesai ? (
-                <div>
-                    <div
-                        className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"
-                    >
-                        <div className="relative w-auto my-6 mx-auto max-w-3xl">
-                            <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
-                                <div className="flex items-start justify-between p-5 border-b border-dashed border-gray-300 rounded-t align-middle">
-                                    <h3 className="text-3xl font-semibold mt-1">
-                                        <FontAwesomeIcon icon={faHeartbeat} className="text-red-500 mr-2"></FontAwesomeIcon> Doctor Shreyas Desai
-                                    </h3>
-                                    <button
-                                        className="p-1 ml-auto bg-gray-200 hover:bg-red-500 rounded border-0 text-black float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
-                                        onClick={() => setShowDesai(false)}
-                                    >
-                                        <span className="text-gray-500 hover:text-white z-50 h-6 w-6 text-2xl block outline-none focus:outline-none">
-                                            ×
-                                        </span>
-                                    </button>
-                                </div>
-                                {/*body*/}
-                                <div className="mt-2 mb-4">
-                                    <span className="inline-block py-4 px-8 text-gray-500 text-lg">
-                                        <img className="inline-block w-60 mr-8 mb-3 object-cover float-left h-60 rounded-full" src="./desai.jpeg" alt="avatar" />
-                                        <h3 className="text-2xl font-semibold mt-1">
-                                            About Dr. Desai
-                                        </h3>
-                                        <p className="mb-4 mt-2">Shreyas Desai is certified in Internal Medicine and Geriatrics. He has been in practice for 45 years practicing at Methodist, St. Mary, and Pinnacle hospitals. </p>
-
-                                        <p className="mb-4 mt-2">He completed his training in Internal Medicine from Mount Sinai University. He went on to complete his Oncology training at Northwestern University.
-                                        </p>
-
-                                        <p className="mb-4 mt-2">
-                                            Dr. Desai has experience in internal medicine, geriatric medicine, and the treatment of substance abuse. He places high value on individualized and continuity of care, strong family values, and preventative medicine.
-                                        </p>
-                                        <p className="mb-4 mt-2">
-                                            Dr. Desai currently resides in Northwest Indiana. He is a father and grandfather with 5 grandchildren and 3 children. His children also work in medicine and law. Being part of the community has inspired him to make a positive impact on others and their health.
-                                        </p>
-                                    </span>
-                                </div>
-
-                                {/*footer*/}
-                            </div>
-                        </div>
-                    </div>
-                    <div className="opacity-40 fixed inset-0 z-40 bg-black"></div>
-                </div>
-            ) : null}
-
-            {showChristina ? (
-                <div>
-                    <div
-                        className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"
-                    >
-                        <div className="relative w-auto my-8 mx-auto max-w-3xl">
-                            <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
-                                <div className="flex items-start justify-between p-5 border-b border-dashed border-gray-300 rounded-t align-middle">
-                                    <h3 className="text-3xl font-semibold mt-1">
-                                        <FontAwesomeIcon icon={faHeartbeat} className="text-red-500 mr-2"></FontAwesomeIcon> Christina Connelly, NP
-                                    </h3>
-                                    <button
-                                        className="p-1 ml-auto bg-gray-200 hover:bg-red-500 rounded border-0 text-black float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
-                                        onClick={() => setShowChristina(false)}
-                                    >
-                                        <span className="text-gray-500 hover:text-white z-50 h-6 w-6 text-2xl block outline-none focus:outline-none">
-                                            ×
-                                        </span>
-                                    </button>
-                                </div>
-                                {/*body*/}
-                                <div className="mt-2">
-                                    <span className="inline-block py-4 px-8 text-gray-500 text-lg">
-                                        <img className="inline-block w-60 mr-8 mb-3 object-cover float-left h-60 rounded-full" src="./christina.jpeg" alt="avatar" />
-                                        <h3 className="text-2xl font-semibold mt-1">
-                                            About Dr. Connelly
-                                        </h3>
-                                        <p className="mb-4 mt-2">Christina Connelly is a certified <strong><i>Family Nurse Practitioner</i></strong>. She obtained her <i>Bachelor’s Degree</i> in Nursing from <strong>Indiana University</strong>. She went on to complete her <i>Master’s Degree</i> at <strong>Indiana Wesleyan University</strong> graduating in 2015. </p>
-
-                                        <p className="mb-4 mt-2">As a Family Nurse Practitioner, she has practiced in a variety of inpatient and outpatient settings. She has also integrated telemedicine into her practice. Christina has experience in critical care, as well as a special focus in obesity medicine, the treatment of opiate dependence, and geriatric medicine.
-                                            She places high value on individualized care, strong family values, and preventative medicine.</p>
-
-                                        <p className="mb-4 mt-2">Christina was born and raised in Northwest Indiana and currently lives in Valparaiso with her husband and youngest daughter. Being part of the community herself, has inspired her to make a positive impact on others and their health.</p>
-                                        <p className="mb-4 mt-2">In her spare time Christina enjoys spending time with her eldest sons, exploring the Dunes State Park, traveling, snorkeling and exercise of any kind.
-                                        </p>
-                                    </span>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="opacity-40 fixed inset-0 z-40 bg-black"></div>
-                </div>
-            ) : null}
-
             <Footer />
         </div>
     );
